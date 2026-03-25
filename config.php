e3b11038441c9ccf8220620570b55eac74a79c8d
MUHAMMADULLAH1991 <muhammadullahsaify859658@gmail.com>
2026-03-25 15:50:54 +0600

    Read is done

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  $connections = new mysqli('localhost', 'root', '', 'school_mgt_system_20');
+
+//   if($connections == true){
+//     echo "Connection is successfull";
+//   }
+//   else{
+//     echo "Connection Failed";
+//   }
+?>
---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+  include 'config.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SMS</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="create.php">Add New Student</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Class</th>
+                    <th scope="col">Roll</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Blood Group</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $query = 'SELECT * FROM students';
+                    $student = mysqli_query($connections, $query); //student data
+
+                    $serialNumber = 1;
+
+                    while($row = mysqli_fetch_assoc($student)){
+
+                        $id = $row['id'];
+                        $name = $row['name'];
+                        $class = $row['class'];
+                        $roll = $row['roll'];
+                        $email = $row['email'];
+                        $phone = $row['phone'];
+                        $bloodgroup = $row['blood'];
+                        $address = $row['address'];
+
+                        echo '<tr>
+                            <th scope="row">'.$serialNumber.'</th>
+                            <td>'.$name.'</td>
+                            <td>'.$class.'</td>
+                            <td>'.$roll.'</td>
+                            <td>'.$email.'</td>
+                            <td>'.$phone.'</td>
+                            <td>'.$bloodgroup.'</td>
+                            <td>'.$address.'</td>
+                            <td>
+                                <a href="#" class="btn btn-info">Edit</a>
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>';
+
+                        $serialNumber++;
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
